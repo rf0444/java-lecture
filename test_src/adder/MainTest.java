46ab9ad455830a0a1c743fe0fbf570986d8ce3f4
rf <rf0444@gmail.com>
2011-09-11 18:27:12 +0900

    改行コードとエンコードを、Systemのプロパティから取ってくるように修正

--- test_src/adder/MainTest.java
+++ test_src/adder/MainTest.java
@@ -10,19 +10,20 @@ import java.util.Scanner;
 import org.junit.Test;
 
 public class MainTest {
-	private static final String LN = String.format("%n");
+	private static final String LN = System.getProperty("line.separator");
+	private static final String ENCODING = System.getProperty("sun.jnu.encoding");
 	@Test
 	public void mainを実行して結果を確認() throws Exception {
 		ProcessBuilder pb = new ProcessBuilder("java", "-cp", "bin/", "adder.Main");
 		Process proc = pb.start();
 		
-		Scanner sin = new Scanner(new BufferedReader(new InputStreamReader(proc.getInputStream(), "sjis")));
+		Scanner sin = new Scanner(new BufferedReader(new InputStreamReader(proc.getInputStream(), ENCODING)));
 		StringBuilder bufin = new StringBuilder();
 		while (sin.hasNextLine()) {
 			bufin.append(sin.nextLine()).append(LN);
 		}
 		
-		Scanner serr = new Scanner(new BufferedReader(new InputStreamReader(proc.getErrorStream(), "sjis")));
+		Scanner serr = new Scanner(new BufferedReader(new InputStreamReader(proc.getErrorStream(), ENCODING)));
 		StringBuilder buferr = new StringBuilder();
 		while (serr.hasNextLine()) {
 			buferr.append(serr.nextLine()).append(LN);
