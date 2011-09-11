722bd2bfa559494e48a8690a14893799cddea0e6
rf <rf0444@gmail.com>
2011-09-11 18:25:15 +0900

    ProcessBuilderを使って別プロセスでmainを実行し、その出力と終了コードを取ってくる
    なんとかテストは出来る。

--- test_src/adder/MainTest.java
+++ test_src/adder/MainTest.java
@@ -3,12 +3,35 @@ package adder;
 import static org.hamcrest.core.Is.*;
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 import org.junit.Test;
 
 public class MainTest {
+	private static final String LN = String.format("%n");
 	@Test
-	public void mainを実行して結果を確認してみたい() throws Exception {
-		Main.main(new String[0]);
-		assertThat(1, is(2)); // (´・ω・｀)
+	public void mainを実行して結果を確認() throws Exception {
+		ProcessBuilder pb = new ProcessBuilder("java", "-cp", "bin/", "adder.Main");
+		Process proc = pb.start();
+		
+		Scanner sin = new Scanner(new BufferedReader(new InputStreamReader(proc.getInputStream(), "sjis")));
+		StringBuilder bufin = new StringBuilder();
+		while (sin.hasNextLine()) {
+			bufin.append(sin.nextLine()).append(LN);
+		}
+		
+		Scanner serr = new Scanner(new BufferedReader(new InputStreamReader(proc.getErrorStream(), "sjis")));
+		StringBuilder buferr = new StringBuilder();
+		while (serr.hasNextLine()) {
+			buferr.append(serr.nextLine()).append(LN);
+		}
+		
+		int exitCode = proc.waitFor();
+		
+		assertThat(bufin.toString(), is("15"+LN));
+		assertThat(buferr.toString(), is("エラー : hoge"+LN));
+		assertThat(exitCode, is(9));
 	}
 }
