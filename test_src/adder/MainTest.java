360c8b2d647bd3a10101bec9a3a09f20a1a8c6af
rf <rf0444@gmail.com>
2011-09-11 18:48:26 +0900

    スレッドを複数使って、それぞれのストリームを消化してやるよう修正
    終了コードの取得も別スレッドでやってみた。
    これで途中で止まらずにうまくテストが動いてくれる。

--- test_src/adder/MainTest.java
+++ test_src/adder/MainTest.java
@@ -1,38 +1,58 @@
 package adder;
 
-import static org.hamcrest.core.Is.*;
-import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import org.junit.Test;
 
 public class MainTest {
 	private static final String LN = System.getProperty("line.separator");
 	private static final String ENCODING = System.getProperty("sun.jnu.encoding");
+	private static final class ProcReader implements Callable<String> {
+		private final InputStream in;
+		private ProcReader(InputStream in) {
+			this.in = in;
+		}
+		public String call() throws Exception {
+			Scanner s = new Scanner(new BufferedReader(new InputStreamReader(new BufferedInputStream(in), ENCODING)));
+			StringBuilder buf = new StringBuilder();
+			while (s.hasNextLine()) {
+				buf.append(s.nextLine()).append(LN);
+			}
+			return buf.toString();
+		}
+	}
 	@Test
 	public void mainを実行して結果を確認() throws Exception {
 		ProcessBuilder pb = new ProcessBuilder("java", "-cp", "bin/", "adder.Main");
-		Process proc = pb.start();
+		final Process proc = pb.start();
 		
-		Scanner sin = new Scanner(new BufferedReader(new InputStreamReader(proc.getInputStream(), ENCODING)));
-		StringBuilder bufin = new StringBuilder();
-		while (sin.hasNextLine()) {
-			bufin.append(sin.nextLine()).append(LN);
-		}
+		ExecutorService service = Executors.newFixedThreadPool(3);
+		Future<String> inputRead = service.submit(new ProcReader(proc.getInputStream()));
+		Future<String> errorRead = service.submit(new ProcReader(proc.getErrorStream()));
+		Future<Integer> exitRead = service.submit(new Callable<Integer>() {
+			public Integer call() throws Exception {
+				return proc.waitFor();
+			}
+		});
+		String out = inputRead.get();
+		String err = errorRead.get();
+		int exitCode = exitRead.get();
+		service.shutdown();
 		
-		Scanner serr = new Scanner(new BufferedReader(new InputStreamReader(proc.getErrorStream(), ENCODING)));
-		StringBuilder buferr = new StringBuilder();
-		while (serr.hasNextLine()) {
-			buferr.append(serr.nextLine()).append(LN);
-		}
-		
-		int exitCode = proc.waitFor();
-		
-//		assertThat(bufin.toString(), is("15"+LN));
-//		assertThat(buferr.toString(), is("エラー : hoge"+LN));
+		assertTrue(out.startsWith("15"+LN));
+		assertTrue(err.startsWith("エラー : hoge"+LN));
 		assertThat(exitCode, is(9));
 	}
 }
